ff23f9503ce01e94f6a9996fb539e7db43d1bddf
Adam <adam@igital.co>
2020-04-26 17:24:17 +0000

    Changed to .igital namespace until we have MV apple dev account

--- app/lib/providers/AuthProvider.dart
+++ app/lib/providers/AuthProvider.dart
@@ -1,19 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:provider/provider.dart';
 
 import 'AnalyticsProvider.dart';
 
 class AuthProvider extends ChangeNotifier {
 
-    AnalyticsProvider analytics;
+    final FirebaseAuth _auth = FirebaseAuth.instance;
+    final AnalyticsProvider _analytics;
 
-    AuthProvider(this.analytics) {
-        testEvent();
-    }
+    AuthProvider(this._analytics);
 
     void testEvent() {
         print('TestEvent');
-        analytics.sendEvent('TEST_Event', {
+        _analytics.sendEvent('TEST_Event', {
             "TESTData": 123,
         });
     }
