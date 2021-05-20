ed45a3219bfee3e91420c02d3091e68f83dca6da
Adam <adam@igital.co>
2021-05-20 12:37:09 +0000

    Type

--- frontend/components/Button.tsx
+++ frontend/components/Button.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { ColorKey } from '../styles/theme';
 
 interface ButtonProps {
-	type?: string;
+	type?: 'button' | 'submit' | 'reset';
 	children?: React.ReactNode;
 	color?: ColorKey;
 	onClick?: (event: MouseEvent) => void;
@@ -12,21 +12,21 @@ interface ButtonProps {
 const ButtonStyled = styled.button<{ backgroundColor: ColorKey }>`
 	padding: ${({ theme }) => theme.padding(1.5, 2.5)};
 	user-select: none;
-	border-radius:  ${({ theme }) => theme.borderRadius};
+	border-radius: ${({ theme }) => theme.borderRadius};
 	cursor: pointer;
 	border: none;
 	outline: none;
 	color: ${({ theme, backgroundColor }) => theme.fontColors.bg[backgroundColor]};
 	background-color: ${({ theme, backgroundColor }) => theme.backgroundColors[backgroundColor]};
 	transform: scale(1);
-	transition: all .1s linear;
+	transition: all 0.1s linear;
 	font-weight: bold;
-	
+
 	&:hover {
 		transform: scale(1.05);
 		background-color: ${({ theme, backgroundColor }) => theme.colors[backgroundColor]['600']};
 	}
-	
+
 	&:active {
 		transform: scale(0.998);
 	}
