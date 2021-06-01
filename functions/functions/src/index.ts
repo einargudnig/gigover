482a9c28bc09a1552c35f6098faee0701f296b84
Adam <adam@igital.co>
2021-06-01 17:09:37 +0000

    Renamed the cf to fix some cors cache issue

--- functions/functions/src/index.ts
+++ functions/functions/src/index.ts
@@ -1,33 +1,41 @@
 import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 import * as express from 'express';
-import * as cors from 'cors';
 
 admin.initializeApp();
 
-const corsHandler = cors({ origin: true,  });
-
 interface UserIdResponse {
-    uId: string;
+	uId: string;
 }
 
-export const getUserId = functions.https.onRequest(async (request: express.Request, response: express.Response<UserIdResponse | string>): Promise<any> => {
-    const phoneNumber: string | undefined = request.body.msisdn;
+export const getUserIdForPhoneNumber = functions.https.onRequest(async (request: express.Request, response: express.Response<UserIdResponse | string>): Promise<any> => {
+	const phoneNumber: string | undefined = request.body.msisdn;
+	// Set CORS headers for preflight requests
+	// Allows GETs from any origin with the Content-Type header
+	// and caches preflight response for 3600s
 
-    return corsHandler(request, response, async () => {
-        if (phoneNumber) {
-            functions.logger.info(`Getting user for phone number: ${phoneNumber}`);
-            const user = await admin.auth().getUserByPhoneNumber(`+354${phoneNumber}`);
+	response.set('Access-Control-Allow-Origin', '*');
 
-            if (user) {
-                return response.status(200).send({
-                    uId: user.uid,
-                });
-            }
+	if (request.method === 'OPTIONS') {
+		// Send response to OPTIONS requests
+		response.set('Access-Control-Allow-Methods', 'POST');
+		response.set('Access-Control-Allow-Headers', 'Content-Type');
+		response.set('Access-Control-Max-Age', '3600');
+		return response.status(204).send('');
+	} else {
+		if (phoneNumber) {
+			functions.logger.info(`Getting user for phone number: ${phoneNumber}`);
+			const user = await admin.auth().getUserByPhoneNumber(`+354${phoneNumber}`);
 
-            return response.status(404).send('Not found');
-        } else {
-            return response.status(400).send('Bad request');
-        }
-    });
+			if (user) {
+				return response.status(200).send({
+					uId: user.uid
+				});
+			}
+
+			return response.status(404).send('Not found');
+		} else {
+			return response.status(400).send('Bad request');
+		}
+	}
 });
