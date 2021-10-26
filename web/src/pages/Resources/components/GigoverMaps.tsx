39e5ed3a60e99bec9b50d2610d708dc1243b7c00
Adam Viðarsson <adam@igital.co>
2021-10-26 12:46:52 +0000

    Name instead of opening the modal

--- web/src/pages/Resources/components/GigoverMaps.tsx
+++ web/src/pages/Resources/components/GigoverMaps.tsx
@@ -1,7 +1,7 @@
-import { GoogleMap, Marker, withGoogleMap, withScriptjs } from 'react-google-maps';
-import React, { useContext } from 'react';
+import { GoogleMap, withGoogleMap, withScriptjs } from 'react-google-maps';
+import React from 'react';
 import { Resource } from '../../../models/Resource';
-import { ModalContext } from '../../../context/ModalContext';
+import { GigoverPin } from './GigoverPin';
 
 interface GigoverMapsWithResources {
 	resources?: Resource[];
@@ -9,40 +9,29 @@ interface GigoverMapsWithResources {
 
 const GigoverMaps = withScriptjs(
 	withGoogleMap(({ resources = [] }: GigoverMapsWithResources) => {
-		const [, setModalContext] = useContext(ModalContext);
 		const defaultCenter = { lat: 64.13548, lng: -21.89541 };
 		const resourcesWithGpsCoord = resources.filter(
 			(q) => (q.startLat && q.startLng) || (q.stopLat && q.stopLng)
 		);
 
-		const findLastPos = (
-			resource: Resource
-		): {
-			lng: number;
-			lat: number;
-		} => {
-			return {
-				lng: resource.stopLng || resource.startLng || 0,
-				lat: resource.stopLat || resource.startLat || 0
-			};
-		};
-
 		return (
 			<GoogleMap defaultZoom={8} defaultCenter={defaultCenter}>
 				{resourcesWithGpsCoord.map((r, rIdx) => (
-					<Marker
-						position={findLastPos(r)}
-						key={rIdx}
-						onClick={() => setModalContext({ resources: { resource: r } })}
-						icon={{
-							url: '/img/MarkPin2.svg',
-							// @ts-ignore
-							anchor: new google.maps.Point(16, 32)
-						}}
-					/>
+					<GigoverPin resource={r} key={rIdx} />
+					// <Marker
+					// 	position={findLastPos(r)}
+					// 	key={rIdx}
+					// 	onClick={() => setModalContext({ resources: { resource: r } })}
+					// 	icon={{
+					// 		url: '/img/MarkPin2.svg',
+					// 		// @ts-ignore
+					// 		anchor: new google.maps.Point(16, 32)
+					// 	}}
+					// />
 				))}
 			</GoogleMap>
 		);
 	})
 );
+
 export default GigoverMaps;
---
+++ web/src/pages/Resources/components/GigoverPin.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { Resource } from '../../../models/Resource';
+import { Marker } from 'react-google-maps';
+import InfoBox from 'react-google-maps/lib/components/addons/InfoBox';
+
+export interface GigoverPinProps {
+	resource: Resource;
+}
+
+export const GigoverPin = ({ resource }: GigoverPinProps): JSX.Element => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const findLastPos = (
+		r: Resource
+	): {
+		lng: number;
+		lat: number;
+	} => {
+		return {
+			lng: r.stopLng || r.startLng || 0,
+			lat: r.stopLat || r.startLat || 0
+		};
+	};
+
+	return (
+		<Marker
+			position={findLastPos(resource)}
+			onClick={() => setIsOpen(!isOpen)}
+			icon={{
+				url: '/img/MarkPin2.svg',
+				// @ts-ignore
+				anchor: new google.maps.Point(16, 32)
+			}}
+		>
+			{isOpen && (
+				<InfoBox
+					onCloseClick={() => setIsOpen(!isOpen)}
+					options={{ closeBoxURL: '', enableEventPropagation: true }}
+				>
+					<div style={{ backgroundColor: '#fff', opacity: 1, padding: '12px' }}>
+						<div style={{ fontSize: '16px', color: '#08233B' }}>{resource.name}</div>
+					</div>
+				</InfoBox>
+			)}
+		</Marker>
+	);
+};
