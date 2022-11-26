5cfeecd8af5a10a29559beeea023d624f1234c92
Einar <einargudnig@gmail.com>
2022-11-26 10:01:50 +0000

    fixed typo

--- web/src/components/CommentInputLabel.tsx
+++ web/src/components/CommentInputLabel.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const CommentInputLabel = (): JSX.Element => {
 	return (
 		<small style={{ display: 'inline-block', marginTop: -24, fontStyle: 'italic' }}>
-			You dan drag and drop photos to the comment section. Press ENTER to send your comment.
+			You can drag and drop photos to the comment section. Press ENTER to send your comment.
 			You can tag workers by typing @ and then their name.
 		</small>
 	);
