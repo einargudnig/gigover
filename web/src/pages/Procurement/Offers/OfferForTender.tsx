e5a5daa1aa1542d0d41ac4121dcca0eede74b94d
Einar <einargudnig@gmail.com>
2023-03-25 22:18:55 +0000

    tiny ui updates for offer for tenders

--- web/src/pages/Procurement/Offers/OfferForTender.tsx
+++ web/src/pages/Procurement/Offers/OfferForTender.tsx
@@ -3,11 +3,36 @@ import styled from 'styled-components';
 import { Page } from '../../../components/Page';
 import { Box, Heading, HStack, VStack, Text } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
+import { CardBase } from '../../../components/CardBase';
 import { useGetOfferForTender } from '../../../queries/useGetOfferForTender';
 import { Offer } from '../../../models/Tender';
 import { LoadingSpinner } from '../../../components/LoadingSpinner';
 import { useTenderById } from '../../../queries/useGetTenderById';
 import { Tender } from '../../../models/Tender';
+
+const OfferCardStyled = styled(CardBase)`
+	width: 100%;
+	max-width: 100%;
+	height: auto;
+	display: flex;
+	justify-content: space-between;
+	flex-direction: column;
+	margin-bottom: 8px;
+
+	h3 {
+		margin-bottom: 16px;
+		color: #000;
+	}
+
+	@media screen and (max-width: 768px) {
+		width: 100%;
+	}
+`;
+
+const OfferCardTitle = styled.div`
+	display: flex;
+	justify-content: space-between;
+`;
 
 const Container = styled.div`
 	flex: 1 0;
@@ -49,14 +74,15 @@ export const OfferForTender = (): JSX.Element => {
 										{offer.length === 0 ? (
 											<Text>No offers have been published!</Text>
 										) : (
-											offer?.map((i) => (
-												<>
-													<Text>Notes: {i.notes}</Text>
-													<Text>Offer Id: {i.offerId}</Text>
-													<Text>Tender Id: {i.tenderId}</Text>
-													<Text>Status: {i.status}</Text>
-													<Text>Status Text: {i.statusText}</Text>
-												</>
+											offer?.map((o) => (
+												<OfferCardStyled key={o.tenderId}>
+													<OfferCardTitle>Test</OfferCardTitle>
+													<Text>Notes: {o.notes}</Text>
+													<Text>Offer Id: {o.offerId}</Text>
+													<Text>Tender Id: {o.tenderId}</Text>
+													<Text>Status: {o.status}</Text>
+													<Text>Status Text: {o.statusText}</Text>
+												</OfferCardStyled>
 											))
 										)}
 									</Box>
--- web/src/pages/Procurement/Offers/TenderOffer.tsx
+++ web/src/pages/Procurement/Offers/TenderOffer.tsx
@@ -1,11 +1,30 @@
 import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { OfferInformation } from './components/OfferInformation';
 import { OfferTable } from './components/OfferTable';
 import { OfferIdContext } from '../../../context/OfferIdContext';
+// import { useGetBidderTenders } from '../../../../queries/useGetBidderTenders';
+import { Tender } from '../../../models/Tender';
+
+// type TenderIdParams = {
+// 	tenderId: string;
+// };
+
+// function findTenderById(tenderId: string, bidderTenders: Tender[]): Tender {
+// 	const tender = bidderTenders.find((t) => t.tenderId === Number(tenderId));
+// 	if (!tender) {
+// 		throw new Error(`Tender with id ${tenderId} not found`);
+// 	}
+// 	return tender;
+// }
 
 export const TenderOffer = (): JSX.Element => {
+	// const { tenderId } = useParams<keyof TenderIdParams>() as TenderIdParams;
 	const [offerId, setOfferId] = useState<number>(0); // This will help us update the offerId value for all the components that need it.
-	// const offerId = 33;
+
+	// I should get the bidder tender here and pass the down
+	//! I can't, since I will have to hav access to the tenderId -> I can get it from the URL here instead of in the component
+
 	return (
 		<>
 			<OfferIdContext.Provider value={{ offerId, setOfferId }}>
