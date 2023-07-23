6c408bcdf051961002bfafe37739879dec921910
Einar <einargudnig@gmail.com>
2023-07-23 09:40:44 +0000

    Tender file now looks better

--- web/src/pages/Files/new/components/TenderFile.tsx
+++ web/src/pages/Files/new/components/TenderFile.tsx
@@ -1,11 +1,48 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useGetTenderById } from '../../../../queries/useGetTenderById';
+import { LoadingSpinner } from '../../../../components/LoadingSpinner';
+import { VStack, Text } from '@chakra-ui/react';
+import { GigoverFile } from '../../components/File';
 
 export const TenderFile = (): JSX.Element => {
-	const { tenderId } = useParams();
-	const { data } = useGetTenderById(Number(tenderId));
+	const params = useParams();
+	const tenderId = params.tenderId ? params.tenderId : -1;
+	const { data, isLoading, isError, error } = useGetTenderById(Number(tenderId));
 	// console.log(data, 'DATA');
-	// const tenderDocuments = data?.tender.documents;
-	return <div>TenderFile</div>;
+	const tenderDocuments = data?.tender.documents;
+
+	if (isLoading) {
+		return <LoadingSpinner />;
+	}
+
+	if (isError && error) {
+		console.log(error);
+		return <div>Error</div>;
+	}
+
+	if (!tenderId) {
+		return <div> Missing Tender Id</div>;
+	}
+
+	return (
+		<>
+			{tenderDocuments!.length > 0 ? (
+				<VStack style={{ width: '100%' }} align={'stretch'} spacing={4} mt={4}>
+					{tenderDocuments!
+						.sort((a, b) => (b.created && a.created ? b.created - a.created : -1))
+						.map((p, pIndex) => (
+							<GigoverFile key={pIndex} file={p} />
+						))}
+				</VStack>
+			) : (
+				<div>
+					<Text>
+						There are no files here. The bidders have not added files to any of the
+						offers.
+					</Text>
+				</div>
+			)}
+		</>
+	);
 };
