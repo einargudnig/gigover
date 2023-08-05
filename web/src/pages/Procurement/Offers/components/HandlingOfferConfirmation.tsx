93b37a68fe86aaed4f0fb1043f78b21178b023d2
Einar <einargudnig@gmail.com>
2023-08-05 10:38:17 -0500

    email template ready

--- web/src/pages/Procurement/Offers/components/HandlingOfferConfirmation.tsx
+++ web/src/pages/Procurement/Offers/components/HandlingOfferConfirmation.tsx
@@ -42,18 +42,39 @@ export const HandlingOfferConfirmation = ({
 	};
 
 	const emailServiceId = process.env.REACT_APP_EMAIL_SERVICE_ID;
-	const emailTemplateId = process.env.REACT_APP_EMAIL_TEMPLATE_ID; //TODO make a new template
+	const emailTemplateId = process.env.REACT_APP_EMAIL_OFFER_TEMPLATE_ID; //TODO make a new template
 	const emailUserId = 'yz_BqW8_gSHEh6eAL'; // this is a public key, so no reason to have it in .env
 
-	const acceptOfferText = 'Your offer has been accepted!';
-	const rejectOfferText = 'Your offer has been rejected!';
+	const handling = {
+		accept: 'accept',
+		reject: 'reject'
+	};
+	const offerStatus = handling[status];
 	// I want to send an email to the bidder when the offer is accepted or rejected
 	// send an email to the bidder
 	const sendEmail = async () => {
 		const templateParams = {
 			offerId: offerId,
-			to_email: email
+			to_email: email,
+			status: offerStatus
 		};
+
+		try {
+			await emailjs
+				.send(emailServiceId!, emailTemplateId!, templateParams!, emailUserId!)
+				.then(
+					function (response) {
+						console.log('SUCCESS!', response.status, response.text);
+					},
+					function (error) {
+						console.log('FAILED...', error);
+					}
+				);
+
+			onClose();
+		} catch (e) {
+			console.log(e);
+		}
 	};
 
 	return (
