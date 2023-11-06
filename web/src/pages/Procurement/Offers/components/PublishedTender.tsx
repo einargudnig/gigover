74ed4a3b081a0191c6693d0dcd37e7aa6a6052c0
Einar <einargudnig@gmail.com>
2023-11-06 05:32:19 +0000

    offerFiles UI for Tender owner is goodgood

--- web/src/pages/Procurement/Offers/components/PublishedTender.tsx
+++ web/src/pages/Procurement/Offers/components/PublishedTender.tsx
@@ -22,19 +22,26 @@ export const PublishedTender = (): JSX.Element => {
 				isOfferLoading={isLoading}
 				showResultsButtons={true}
 			/>
-			{offerDocuments?.length > 0 ? (
-				<VStack style={{ width: '100%' }} align={'stretch'} spacing={4} mt={4}>
-					<Heading size={'md'}>Files added to this offer</Heading>
-					{offerDocuments!
-						.sort((a, b) => (b.created && a.created ? b.created - a.created : -1))
-						.map((p, pIndex) => (
-							<OtherGigoverFile key={pIndex} file={p} />
-						))}
-				</VStack>
+			{offerDocuments ? (
+				offerDocuments.length > 0 ? (
+					<VStack style={{ width: '100%' }} align={'stretch'} spacing={4} mt={4}>
+						<Heading size={'md'}>Files added to this offer</Heading>
+						{offerDocuments!
+							.sort((a, b) => (b.created && a.created ? b.created - a.created : -1))
+							.map((p, pIndex) => (
+								<OtherGigoverFile key={pIndex} file={p} />
+							))}
+					</VStack>
+				) : (
+					<EmptyState
+						title={'No files uploaded'}
+						text={'No files have been uploaded to this offer'}
+					/>
+				)
 			) : (
 				<EmptyState
-					title={'No files uploaded'}
-					text={'No files have been uploaded to this offer'}
+					title={'No files found!'}
+					text={'There might be an error fetching the files. Please contact support.'}
 				/>
 			)}
 		</>
