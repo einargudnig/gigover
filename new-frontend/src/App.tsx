c200f76c1a2311eca2262857b4928f9527e62efa
Einar <einargudnig@gmail.com>
2024-04-19 09:17:08 +0000

    first steps

--- new-frontend/src/App.tsx
+++ new-frontend/src/App.tsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
 
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import { Flex } from '@chakra-ui/react'
+import { Navbar } from './components/navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+      <Navbar />
       <Flex>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -20,9 +19,7 @@ function App() {
       </Flex>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+       
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
