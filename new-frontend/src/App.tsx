4f812ff70479810f4e0e1aa88ae8c47ae1b3ef31
Einar <einargudnig@gmail.com>
2024-04-19 16:12:25 +0000

    grid setup done

--- new-frontend/src/App.tsx
+++ new-frontend/src/App.tsx
@@ -1,34 +1,38 @@
+import styled from 'styled-components';
+import { Navbar } from './components/navbar';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
 
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { Flex } from '@chakra-ui/react'
-import { Navbar } from './components/navbar'
+const Grid = styled.div`
+	display: grid;
+	grid-template-columns: repeat(12, 1fr);
+	grid-gap: 20px;
+	margin: 0 36px;
+	max-width: 1440px;
+`;
 
 function App() {
+	return (
+		<>
+			<Navbar />
+			<Grid>
+				<a href="https://vitejs.dev" target="_blank">
+					<img src={viteLogo} className="logo" alt="Vite logo" />
+				</a>
+				<a href="https://react.dev" target="_blank">
+					<img src={reactLogo} className="logo react" alt="React logo" />
+				</a>
 
-  return (
-    <>
-      <Navbar />
-      <Flex>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </Flex>
-      <h1>Vite + React</h1>
-      <div className="card">
-       
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+				<h1>Vite + React</h1>
+				<div className="card">
+					<p>
+						Edit <code>src/App.tsx</code> and save to test HMR
+					</p>
+				</div>
+				<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+			</Grid>
+		</>
+	);
 }
 
-export default App
+export default App;
