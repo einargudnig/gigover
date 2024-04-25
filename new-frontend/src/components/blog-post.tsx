202815f9f57d6dfec4e330dada0acc762d886ce2
Einar <einargudnig@gmail.com>
2024-04-25 09:28:24 +0000

    from styled to chakra

--- new-frontend/src/components/blog-post.tsx
+++ new-frontend/src/components/blog-post.tsx
@@ -1,111 +1,69 @@
 import { Box, Flex, Text, Image } from '@chakra-ui/react';
 import { Blog } from '../queries/useBlogPosts';
-import styled from 'styled-components';
 
-export const BlogStyled = styled.div`
-	width: 100%;
-	height: 440px;
-	background: #f6eada;
-	display: flex;
-	flex-direction: column;
-	overflow: hidden;
-	opacity: 1;
-	transition: all 0.2s linear;
-
-	&:hover {
-		opacity: 0.9;
-	}
-`;
-
-const BlogImageContainer = styled.div`
-	background: #e5e5e5;
-	width: 100%;
-	height: 200px;
-
-	img {
-		width: 100%;
-		height: 100%;
-		object-fit: cover;
-	}
-`;
-
-const BlogTitle = styled.h3`
-	line-height: 26px;
-  padding: 24px
-	padding-bottom: 0;
-	margin-bottom: 16px;
-`;
-
-const BlogPreviewContainer = styled.div`
-	flex: 1 1;
-	padding: 24px;
-	padding-top: 0;
-`;
-
-const BlogPreview = styled.p`
-	display: -webkit-box;
-	-webkit-line-clamp: 4;
-	-webkit-box-orient: vertical;
-	overflow: hidden;
-	text-overflow: ellipsis;
-	font-size: 14px;
-	line-height: 22px;
-`;
-
-const BlogFooter = styled.div`
-	display: flex;
-	flex: 0 0 30px;
-	color: #000;
-	padding-left: 24px;
-	padding-bottom: 24px;
-	font-weight: bold;
-	font-size: 12px;
-	align-items: flex-end;
-`;
-
-export interface BlogPostProps {
+interface BlogPostProps {
 	blog: Blog;
 }
 
 export const BlogPost = ({ blog }: BlogPostProps): JSX.Element => {
-	console.log({ blog });
 	return (
-		<>
-			{/* <BlogStyled>
-				<BlogImageContainer>
-					<img src={blog.image.url} alt={blog.title} />
-				</BlogImageContainer>
-				<BlogTitle>{blog.title}</BlogTitle>
-				<BlogPreviewContainer>
-					<BlogPreview>{blog.content.text.substr(0, 250)}</BlogPreview>
-				</BlogPreviewContainer>
-				<BlogFooter>By the Gigover Team</BlogFooter>
-			</BlogStyled> */}
-			<Box height={'440px'} width={'100%'} background={'#f6eada'} overflow={'hidden'}>
-				<Flex flexDirection={'column'}>
-					<Box background={'#e5e5e5'} width={'100%'} height={'200px'} overflow={'hidden'}>
-						<Image src={blog.image.url} alt={blog.title} />
-					</Box>
-					<Box
-						lineHeight={'26px'}
-						padding={'24px'}
-						paddingBottom={'0'}
-						marginBottom={'16px'}
-					>
-						<Text fontSize={'lg'}>{blog.title}</Text>
-					</Box>
-					<Box padding={'24px'} paddingTop={'0'}>
-						<Text overflow={'hidden'} textOverflow={'ellipsis'} lineHeight={'22px'}>
-							{blog.content.text.substr(0, 250)}
-						</Text>
-					</Box>
-					<Box color={'#000'} paddingLeft={'24px'} paddingBottom={'24px'}>
-						<Flex alignItems={'flex-end'}>
-							<Text as="b">By the Gigover Team</Text>
-						</Flex>
-					</Box>
-				</Flex>
+		<Box
+			width="100%"
+			height="440px"
+			bg="#f6eada"
+			display="flex"
+			flexDirection="column"
+			overflow="hidden"
+			transition="all 0.2s linear"
+			_hover={{
+				opacity: 0.9
+			}}
+		>
+			<Box bg="#e5e5e5" width="100%" height="200px">
+				<Image
+					src={blog.image.url}
+					alt={blog.title}
+					width="100%"
+					height="100%"
+					objectFit="cover"
+				/>
 			</Box>
-		</>
+			<Text
+				as="h3"
+				lineHeight="26px"
+				paddingX="24px"
+				paddingTop="24px"
+				paddingBottom="0"
+				marginBottom="16px"
+			>
+				{blog.title}
+			</Text>
+			<Flex flex="1 1" paddingX="24px" paddingTop="0">
+				<Text
+					display="-webkit-box"
+					overflow="hidden"
+					textOverflow="ellipsis"
+					fontSize="14px"
+					lineHeight="22px"
+					css={{
+						WebkitLineClamp: 4,
+						WebkitBoxOrient: 'vertical'
+					}}
+				>
+					{blog.content.text.substr(0, 250)}
+				</Text>
+			</Flex>
+			<Flex
+				flex="0 0 30px"
+				alignItems="flex-end"
+				paddingLeft="24px"
+				paddingBottom="24px"
+				color="#000"
+				fontWeight="bold"
+				fontSize="12px"
+			>
+				By the Gigover Team
+			</Flex>
+		</Box>
 	);
 };
