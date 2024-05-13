b16af6d619e9dc30519bb3493285c55d076f3d68
Einar <einargudnig@gmail.com>
2024-05-13 22:54:35 +0000

    adding firebase

--- new-frontend/src/main.tsx
+++ new-frontend/src/main.tsx
@@ -7,6 +7,19 @@ import App from './App.tsx';
 import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { IntercomProvider } from 'react-use-intercom';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyAS-XYBiGnAMdLORL1ctzYgN81pwargt80',
+	authDomain: 'gigover2.firebaseapp.com',
+	databaseURL: 'https://gigover2.firebaseio.com',
+	projectId: 'gigover2',
+	storageBucket: 'gigover2.appspot.com',
+	messagingSenderId: '761785841920',
+	appId: '1:761785841920:web:477d337ae42115c2621be5',
+	measurementId: 'G-C5KYRDS14C'
+};
 
 const ChakraTheme = extendTheme({
 	colors: ChakraThemeColors,
@@ -59,6 +72,10 @@ const INTERCOM_APP_ID = 'jsp3pks1';
 
 // Create a client
 const queryClient = new QueryClient();
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+getAnalytics(app);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
