b46360e2294da2f510bc55d6a39f52a85632e5f9
Einar <einargudnig@gmail.com>
2024-05-16 17:40:47 +0000

    feature grid

--- new-frontend/src/components/features-section.tsx
+++ new-frontend/src/components/features-section.tsx
@@ -1,4 +1,4 @@
-import { Box, VStack, Text, Flex } from '@chakra-ui/react';
+import { Box, VStack, Text, Grid, GridItem } from '@chakra-ui/react';
 import { TaskManagementFeatureIcon } from './icons/task-management';
 import { TimeTrackingFeatureIcon } from './icons/time-tracking';
 import { EstimatesAndReportsFeatureIcon } from './icons/estimates';
@@ -6,51 +6,58 @@ import { EstimatesAndReportsFeatureIcon } from './icons/estimates';
 export const FeaturesSection = () => {
 	return (
 		<Box>
-			<Flex flexDirection={['column', 'row']} gap={3}>
-				<VStack>
-					<Box>
-						<TaskManagementFeatureIcon />
-					</Box>
-					<Box>
-						<Text as="b">
-							Timely, budget-friendly, and high-quality project completion
-						</Text>
-						<Text>
-							Project management ensures that property management projects are
-							completed on time, within budget, and to the highest standards, reducing
-							delays and cost overruns.
-						</Text>
-					</Box>
-				</VStack>
-				<VStack>
-					<Box>
-						<TimeTrackingFeatureIcon />
-					</Box>
-					<Box>
-						<Text as="b">
-							Effective stakeholder communication and progress tracking
-						</Text>
-						<Text>
-							By facilitating ongoing communication with key stakeholders and tracking
-							progress, project management keeps everyone aligned and focused on the
-							end result, ensuring smooth collaboration and transparency.
-						</Text>
-					</Box>
-				</VStack>
-				<VStack>
-					<Box>
-						<EstimatesAndReportsFeatureIcon />
-					</Box>
-					<Box>
-						<Text as="b">Informed decision-making</Text>
-						<Text>
-							Project management eliminates rework and mistakes by providing the team
-							with all necessary information, enabling them to make informed decisions
-							and maintain project efficiency.
-						</Text>
-					</Box>
-				</VStack>
-			</Flex>
+			<Grid templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(3, 1fr)' }} gap={3}>
+				<GridItem colSpan={1}>
+					<VStack>
+						<Box marginTop={[10, 0]} backgroundColor={'gray.200'} rounded={'md'} p={10}>
+							<TaskManagementFeatureIcon />
+						</Box>
+						<Box marginTop={[4, 0]}>
+							<Text as="b">
+								Timely, budget-friendly, and high-quality project completion
+							</Text>
+							<Text>
+								Project management ensures that property management projects are
+								completed on time, within budget, and to the highest standards,
+								reducing delays and cost overruns.
+							</Text>
+						</Box>
+					</VStack>
+				</GridItem>
+				<GridItem colSpan={1}>
+					<VStack>
+						<Box marginTop={[10, 0]} backgroundColor={'gray.200'} rounded={'md'} p={10}>
+							<TimeTrackingFeatureIcon />
+						</Box>
+						<Box marginTop={[4, 0]}>
+							<Text as="b">
+								Effective stakeholder communication and progress tracking
+							</Text>
+							<Text>
+								By facilitating ongoing communication with key stakeholders and
+								tracking progress, project management keeps everyone aligned and
+								focused on the end result, ensuring smooth collaboration and
+								transparency.
+							</Text>
+						</Box>
+					</VStack>
+				</GridItem>
+				<GridItem colSpan={1}>
+					<VStack>
+						<Box marginTop={[10, 0]} backgroundColor={'gray.200'} rounded={'md'} p={10}>
+							<EstimatesAndReportsFeatureIcon />
+						</Box>
+						<Box marginTop={[4, 0]}>
+							<Text as="b">Informed decision-making</Text>
+							<Text>
+								Project management eliminates rework and mistakes by providing the
+								team with all necessary information, enabling them to make informed
+								decisions and maintain project efficiency.
+							</Text>
+						</Box>
+					</VStack>
+				</GridItem>
+			</Grid>
 		</Box>
 	);
 };
