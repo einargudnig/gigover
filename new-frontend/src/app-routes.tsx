6c47878e947b35af29e1cf902ad17abc76b475d6
Einar <einargudnig@gmail.com>
2024-05-21 16:34:51 +0000

    starting work on new pricing page

--- new-frontend/src/app-routes.tsx
+++ new-frontend/src/app-routes.tsx
@@ -2,7 +2,8 @@ import { Route, Routes } from 'react-router-dom';
 import { Layout } from './components/layout';
 import { Landing } from './pages/landing';
 import { Features } from './pages/features';
-import { Pricing } from './pages/pricing';
+// import { Pricing } from './pages/pricing';
+import { NewPricing } from './pages/new-pricing';
 import { BlogPosts } from './pages/blog-posts';
 import { BlogPost } from './pages/blog-post';
 import { MobileApp } from './pages/mobile-app';
@@ -14,7 +15,8 @@ export const AppRoutes = (): JSX.Element => {
 			<Route path="/" element={<Layout />}>
 				<Route index element={<Landing />} />
 				<Route path="features" element={<Features />} />
-				<Route path="pricing" element={<Pricing />} />
+				{/* <Route path="pricing" element={<Pricing />} /> */}
+				<Route path="pricing" element={<NewPricing />} />
 				<Route path="blog" element={<BlogPosts />} />
 				<Route path="blog/:id/:slug" element={<BlogPost />} />
 				<Route path="mobile-app" element={<MobileApp />} />
---
+++ new-frontend/src/pages/new-pricing.tsx
@@ -0,0 +1,12 @@
+import { Helmet } from 'react-helmet-async';
+
+export const NewPricing = (): JSX.Element => {
+	return (
+		<>
+			<Helmet>
+				<title>Gigover | Pricing</title>
+				<link rel="canonical" href="https://www.gigover.com/pricing" />
+			</Helmet>
+		</>
+	);
+};
