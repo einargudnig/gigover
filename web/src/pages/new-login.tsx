c16c59c498d6dfc9982226ea063495c610cba156
Einar <einargudnig@gmail.com>
2024-06-02 22:22:37 +0000

    building log in form

--- web/src/pages/new-login.tsx
+++ web/src/pages/new-login.tsx
@@ -26,29 +26,6 @@ import { Firebase } from '../firebase/firebase';
 import { FirebaseContext } from '../firebase/FirebaseContext';
 
 export const NewLogin = (): JSX.Element => {
-	const [loading, setLoading] = useState<boolean>(false);
-	const firebase: Firebase = useContext(FirebaseContext);
-
-	const { register, handleSubmit } = useForm<{ email: string; password: string }>({
-		defaultValues: {
-			email: '',
-			password: ''
-		}
-	});
-
-	// this function calls the firebase to log in the user with the provided credentials
-	const loginWithCredentials = async (email: string, password: string) => {
-		console.log({ email, password });
-		try {
-			setLoading(true);
-			await firebase.auth.signInWithEmailAndPassword(email, password);
-			setLoading(false);
-		} catch (e) {
-			console.error(e);
-			setLoading(false);
-		}
-	};
-
 	return (
 		<Container maxW="lg" py={{ base: '12', md: '24' }} px={{ base: '0', sm: '8' }}>
 			<Tabs
@@ -76,62 +53,91 @@ export const NewLogin = (): JSX.Element => {
 	);
 };
 
-const LoginForm = () => (
-	<Stack spacing="8">
-		<Stack spacing="6">
-			<Flex justifyContent={'center'} alignItems={'center'}>
-				<GigoverLogo color={'black'} />
-			</Flex>
-			<Stack spacing={{ base: '2', md: '3' }} textAlign="center">
-				<Heading size={{ base: 'xs', md: 'sm' }}>Log in to your account</Heading>
-				<Text color="fg.muted">
-					Don&apos;t have an account? <Link href="#">Sign up</Link>
-				</Text>
-			</Stack>
-		</Stack>
-		<Box
-			py={{ base: '0', sm: '8' }}
-			px={{ base: '4', sm: '10' }}
-			bg={{ base: 'transparent', sm: 'bg.surface' }}
-			boxShadow={{ base: 'none', sm: 'md' }}
-			borderRadius={{ base: 'none', sm: 'xl' }}
-		>
-			<Stack spacing="6">
-				<Stack spacing="5">
-					<form>
-						<FormControl>
-							<FormLabel htmlFor="email">Email</FormLabel>
-							<Input id="email" type="email" />
-						</FormControl>
+const LoginForm = () => {
+	const [loading, setLoading] = useState<boolean>(false);
+	const firebase: Firebase = useContext(FirebaseContext);
 
-						<FormControl>
-							<FormLabel htmlFor="password">Password</FormLabel>
-							<Input id="password" type="password" />
-						</FormControl>
-						<Stack spacing={'6'} marginTop={'4'}>
-							<Button>Log in</Button>
-						</Stack>
-					</form>
-				</Stack>
-				<Stack spacing="6">
-					<HStack justify="center">
-						<Button variant="text" size="sm">
-							Forgot password?
-						</Button>
-					</HStack>
-					<HStack>
-						<Divider />
-						<Text textStyle="sm" whiteSpace="nowrap" color="fg.muted">
-							or continue with
-						</Text>
-						<Divider />
-					</HStack>
-					{/* OAuthButtonGroup can be added here */}
+	const { register, handleSubmit } = useForm<{ email: string; password: string }>({
+		defaultValues: {
+			email: '',
+			password: ''
+		}
+	});
+
+	// this function calls the firebase to log in the user with the provided credentials
+	const loginWithCredentials = async (email: string, password: string) => {
+		console.log({ email, password });
+		// try {
+		// 	setLoading(true);
+		// 	await firebase.auth.signInWithEmailAndPassword(email, password);
+		// 	setLoading(false);
+		// } catch (e) {
+		// 	console.error(e);
+		// 	setLoading(false);
+		// }
+	};
+
+	return (
+		<Stack spacing="8">
+			<Stack spacing="6">
+				<Flex justifyContent={'center'} alignItems={'center'}>
+					<GigoverLogo color={'black'} />
+				</Flex>
+				<Stack spacing={{ base: '2', md: '3' }} textAlign="center">
+					<Heading size={{ base: 'xs', md: 'sm' }}>Log in to your account</Heading>
+					<Text color="fg.muted">
+						Don&apos;t have an account? <Link href="#">Sign up</Link>
+					</Text>
 				</Stack>
 			</Stack>
-		</Box>
-	</Stack>
-);
+			<Box
+				py={{ base: '0', sm: '8' }}
+				px={{ base: '4', sm: '10' }}
+				bg={{ base: 'transparent', sm: 'bg.surface' }}
+				boxShadow={{ base: 'none', sm: 'md' }}
+				borderRadius={{ base: 'none', sm: 'xl' }}
+			>
+				<Stack spacing="6">
+					<Stack spacing="5">
+						<form
+							onSubmit={handleSubmit(async (values) => {
+								loginWithCredentials(values.email, values.password);
+							})}
+						>
+							<FormControl>
+								<FormLabel htmlFor="email">Email</FormLabel>
+								<Input type="email" {...register('email')} />
+							</FormControl>
+
+							<FormControl>
+								<FormLabel htmlFor="password">Password</FormLabel>
+								<Input type="password" {...register('password')} />
+							</FormControl>
+							<Stack spacing={'6'} marginTop={'4'}>
+								<Button isLoading={loading}>Log in</Button>
+							</Stack>
+						</form>
+					</Stack>
+					<Stack spacing="6">
+						<HStack justify="center">
+							<Button variant="text" size="sm">
+								Forgot password?
+							</Button>
+						</HStack>
+						<HStack>
+							<Divider />
+							<Text textStyle="sm" whiteSpace="nowrap" color="fg.muted">
+								or continue with
+							</Text>
+							<Divider />
+						</HStack>
+						{/* OAuthButtonGroup can be added here */}
+					</Stack>
+				</Stack>
+			</Box>
+		</Stack>
+	);
+};
 
 const SignUpForm = () => (
 	<Stack spacing="6">
