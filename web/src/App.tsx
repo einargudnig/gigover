1cd9810f64ffbf405192020bdb732f90073d3289
Einar <einargudnig@gmail.com>
2024-06-05 09:06:14 +0000

    ready for merge

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,6 @@
 import 'normalize.css';
 import React, { useContext, useEffect, useMemo, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Login } from './pages/Login';
 import { FirebaseContext } from './firebase/FirebaseContext';
 import { Firebase } from './firebase/firebase';
 import { IUserProfile } from './models/UserProfile';
@@ -107,7 +106,6 @@ const App = ({
 				</QueryParamProvider>
 			) : (
 				<Routes>
-					{/* <Route path={'*'} element={<Login />} /> */}
 					<Route path={'*'} element={<NewLogin />} />
 				</Routes>
 			)}
