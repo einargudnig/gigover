610fe4960bcfd5ba39cf36c23ed3ea5b61e24fcd
Einar <einargudnig@gmail.com>
2025-03-26 21:35:34 +0000

    adde publish button that I had forgotten

--- web/src/pages/Procurement/ClientBids/components/BidDetails.tsx
+++ web/src/pages/Procurement/ClientBids/components/BidDetails.tsx
@@ -1,9 +1,10 @@
 import { ArrowBackIcon } from '@chakra-ui/icons';
-import { Box, Button, Flex, Text } from '@chakra-ui/react';
+import { Box, Button, Flex, Text, useToast } from '@chakra-ui/react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { Center } from '../../../../components/Center';
 import { LoadingSpinner } from '../../../../components/LoadingSpinner';
 import { Bid } from '../../../../models/Tender';
+import { usePublishBid } from '../../../../mutations/procurement/client-bids/usePublishBid';
 import { useGetBidById } from '../../../../queries/procurement/client-bids/useGetBidById';
 import { BidIdHeader } from './BidIdHeader';
 import { BidIdTable } from './BidIdTable';
@@ -60,6 +61,60 @@ function UnpublishedBid({ bid }: { bid: Bid }) {
 		<Box p={4}>
 			<BidIdHeader bid={bid} />
 			<BidIdTable bid={bid} />
+			<HandleBid bid={bid} />
 		</Box>
+	);
+}
+
+function HandleBid({ bid }: { bid: Bid }) {
+	const { mutateAsync: publishBid, isLoading: isPublishLoading } = usePublishBid();
+	const toast = useToast();
+
+	const handlePublish = async () => {
+		const publishBidBody = {
+			bidId: Number(bid.bidId)
+		};
+		if (bid !== undefined) {
+			try {
+				await publishBid(publishBidBody);
+				toast({
+					title: 'Bid published',
+					description: 'Now you can invite people to send offers to your bid!',
+					status: 'success',
+					duration: 2000,
+					isClosable: true
+				});
+			} catch (error) {
+				// console.log('ERROR', { error });
+				toast({
+					title: 'Error',
+					description: 'Something went wrong when we tried to publish your bid.',
+					status: 'error',
+					duration: 3000,
+					isClosable: true
+				});
+			}
+		} else {
+			toast({
+				title: 'Error',
+				description: 'Something went wrong when we tried to publish your bid.',
+				status: 'error',
+				duration: 5000,
+				isClosable: true
+			});
+		}
+	};
+
+	return (
+		<Flex justify={'end'} pr={2} pt={2}>
+			<Button
+				colorScheme={'black'}
+				variant={'outline'}
+				onClick={handlePublish}
+				isLoading={isPublishLoading}
+			>
+				Publish Bid
+			</Button>
+		</Flex>
 	);
 }
