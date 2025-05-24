72d3c5a36b185969b667f5eedfdd1a35b07bb979
Einar <einargudnig@gmail.com>
2025-05-24 22:10:35 +0000

    fixing type error

--- web/src/queries/useRegister.ts
+++ web/src/queries/useRegister.ts
@@ -1,10 +1,10 @@
 import { useMutation } from '@tanstack/react-query';
-import { ErrorResponse, ErrorTypes } from '../models/ErrorResponse';
-import { ApiService } from '../services/ApiService';
 import axios from 'axios';
-import { Firebase } from '../firebase/firebase';
 import { useContext } from 'react';
 import { FirebaseContext } from '../firebase/FirebaseContext';
+import { Firebase } from '../firebase/firebase';
+import { ErrorTypes } from '../models/ErrorResponse';
+import { ApiService } from '../services/ApiService';
 
 export interface RegistrationData {
 	name: string;
@@ -20,28 +20,28 @@ export const useRegister = () => {
 	const firebase: Firebase = useContext(FirebaseContext);
 
 	return useMutation({
-        mutationFn: async (variables) =>
+		mutationFn: async (variables: RegistrationData) =>
 			await axios.post(ApiService.registerUser, variables, {
 				withCredentials: true
 			}),
 
-        onSuccess: async (res) => {
-            // Refresh Firebase Credentials
-            if (res.data.errorCode === ErrorTypes.OK) {
-                // @ts-ignore
-                const cloneCurrentUser = Object.assign(firebase.auth.currentUser, {});
+		onSuccess: async (res) => {
+			// Refresh Firebase Credentials
+			if (res.data.errorCode === ErrorTypes.OK) {
+				// @ts-ignore
+				const cloneCurrentUser = Object.assign(firebase.auth.currentUser, {});
 
-                await firebase.auth.signOut();
-                // @ts-ignore
-                await firebase.auth.updateCurrentUser(cloneCurrentUser);
-            } else {
-                throw new Error(
-                    'Could not register user, Code: ' +
-                        res.data.errorCode +
-                        ' Message: ' +
-                        res.data.errorText
-                );
-            }
-        }
-    });
+				await firebase.auth.signOut();
+				// @ts-ignore
+				await firebase.auth.updateCurrentUser(cloneCurrentUser);
+			} else {
+				throw new Error(
+					'Could not register user, Code: ' +
+						res.data.errorCode +
+						' Message: ' +
+						res.data.errorText
+				);
+			}
+		}
+	});
 };
