dd728a772dee8d1d85033f0d3f03522b3cb43b2e
Einar <einargudnig@gmail.com>
2025-05-26 22:21:12 +0000

    remove styled components

--- web/src/pages/Property/Property.tsx
+++ web/src/pages/Property/Property.tsx
@@ -1,24 +1,8 @@
-import { Flex, Grid, GridItem, HStack, Text } from '@chakra-ui/react';
-import styled from 'styled-components';
+import { Flex, Grid, GridItem, HStack, LinkBox, LinkOverlay, Text } from '@chakra-ui/react';
 import { CardBaseLink } from '../../components/CardBase';
 import { Center } from '../../components/Center';
 import { LoadingSpinner } from '../../components/LoadingSpinner';
 import { useGetProperties } from '../../queries/properties/useGetPoperties';
-
-const PropertyCardStyled = styled(CardBaseLink)`
-	width: 100%;
-	max-width: 100%;
-	height: auto;
-
-	h3 {
-		margin-bottom: 16px;
-		color: #000;
-	}
-
-	@media screen and (max-width: 768px) {
-		width: 100%;
-	}
-`;
 
 export const Property = (): JSX.Element => {
 	const { data, isPending, isFetching } = useGetProperties();
@@ -40,47 +24,62 @@ export const Property = (): JSX.Element => {
 							.slice()
 							.reverse()
 							.map((property) => (
-								<PropertyCardStyled
-									key={property.propertyId}
+								<LinkBox
+									as={CardBaseLink}
 									to={`/property/${property.propertyId}`}
+									key={property.propertyId}
+									w="100%"
+									maxW="100%"
+									h="auto"
+									sx={{
+										h3: {
+											marginBottom: '16px',
+											color: '#000'
+										},
+										'@media screen and (max-width: 768px)': {
+											width: '100%'
+										}
+									}}
 								>
-									<Flex direction={'column'}>
-										<Grid templateColumns="repeat(4, 1fr)" gap={1}>
-											<GridItem colSpan={2}>
-												<HStack>
-													<Text color={'black'}>Name:</Text>
-													<Text>{property.name}</Text>
-												</HStack>
-												<HStack>
-													<Text color={'black'}>Address: </Text>
-													<Text>{property.address}</Text>
-												</HStack>
-												<HStack>
-													<Text color={'black'}>City: </Text>
-													<Text>{property.city}</Text>
-												</HStack>
-											</GridItem>
-											<GridItem colSpan={2}>
-												<HStack>
-													<Text color={'black'}>Zip Code: </Text>
-													<Text>{property.zipCode}</Text>
-												</HStack>
-												<HStack>
-													<Text color={'black'}>Country:</Text>
-													<Text>{property.country}</Text>
-												</HStack>
-												<HStack>
-													<Text color={'black'}>Size:</Text>
-													<Text>{property.size}</Text>
-												</HStack>
-												<HStack>
-													<Text color={'black'}>Type:</Text>
-													<Text>{property.type}</Text>
-												</HStack>
-											</GridItem>
-										</Grid>
-									</Flex>
-								</PropertyCardStyled>
+									<LinkOverlay href={`/property/${property.propertyId}`}>
+										<Flex direction={'column'}>
+											<Grid templateColumns="repeat(4, 1fr)" gap={1}>
+												<GridItem colSpan={2}>
+													<HStack>
+														<Text color={'black'}>Name:</Text>
+														<Text>{property.name}</Text>
+													</HStack>
+													<HStack>
+														<Text color={'black'}>Address: </Text>
+														<Text>{property.address}</Text>
+													</HStack>
+													<HStack>
+														<Text color={'black'}>City: </Text>
+														<Text>{property.city}</Text>
+													</HStack>
+												</GridItem>
+												<GridItem colSpan={2}>
+													<HStack>
+														<Text color={'black'}>Zip Code: </Text>
+														<Text>{property.zipCode}</Text>
+													</HStack>
+													<HStack>
+														<Text color={'black'}>Country:</Text>
+														<Text>{property.country}</Text>
+													</HStack>
+													<HStack>
+														<Text color={'black'}>Size:</Text>
+														<Text>{property.size}</Text>
+													</HStack>
+													<HStack>
+														<Text color={'black'}>Type:</Text>
+														<Text>{property.type}</Text>
+													</HStack>
+												</GridItem>
+											</Grid>
+										</Flex>
+									</LinkOverlay>
+								</LinkBox>
 							))
 					)}
 				</>
