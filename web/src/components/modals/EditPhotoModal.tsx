3d7bad44d239ed0194cedd625313556326e0fb0d
Einar <einargudnig@gmail.com>
2025-06-04 07:48:39 +0000

    improving editphotoModal

--- web/src/components/modals/EditPhotoModal.tsx
+++ web/src/components/modals/EditPhotoModal.tsx
@@ -6,11 +6,15 @@ import {
 	Heading,
 	IconButton,
 	Modal,
+	ModalBody,
+	ModalCloseButton,
+	ModalContent,
+	ModalHeader,
+	ModalOverlay,
 	Spacer,
 	Tag,
 	Text,
-	VStack,
-	useDisclosure
+	VStack
 } from '@chakra-ui/react';
 import { DateTime } from 'luxon';
 import React, { useContext, useEffect, useState } from 'react';
@@ -46,6 +50,7 @@ interface FileSidebarProps {
 	file: ProjectImage;
 	projectId?: number;
 	moveFile: (direction: 'left' | 'right') => void;
+	isOpen: boolean;
 }
 
 export interface IImageDot extends ICommentChord {
@@ -72,8 +77,12 @@ export interface ICommentComment {
 	userName: string;
 }
 
-export const EditPhotoModal = ({ file, moveFile }: FileSidebarProps): JSX.Element => {
-	const { isOpen, onOpen, onClose } = useDisclosure();
+export const EditPhotoModal = ({
+	file,
+	moveFile,
+	isOpen,
+	onClose
+}: FileSidebarProps): JSX.Element => {
 	const Icon = GigoverFileIconForType(file.type);
 	const [activePoint, setActivePoint] = useState(-1);
 	const params = useParams();
@@ -161,239 +170,237 @@ export const EditPhotoModal = ({ file, moveFile }: FileSidebarProps): JSX.Elemen
 	}, [projects, params.projectId]);
 
 	return (
-		<>
-			<Modal isOpen={true} onClose={() => onClose()}>
-				<VStack
-					align={'stretch'}
-					width={{
-						base: '100%', // 0-48em
-						lg: '80vw'
-					}}
-					style={{ flex: 1, height: '100%' }}
-				>
-					<HStack justify={'space-between'} align={'center'} spacing={4}>
+		<Modal isOpen={isOpen} onClose={onClose} size="6xl" scrollBehavior="inside">
+			<ModalOverlay />
+			<ModalContent>
+				<ModalHeader>
+					<HStack align={'center'} spacing={4}>
 						<div>
 							<Icon />
 						</div>
-						<Text isTruncated={true}>
-							<span
-								contentEditable={true}
-								suppressContentEditableWarning
-								onBlur={onChangeFileName}
-							>
-								{file.name}
-							</span>
-						</Text>
+						<Text isTruncated={true}>{file.name}</Text>
 					</HStack>
-					<Divider />
-					{
-						<Flex
-							minHeight={'60vh'}
-							direction={{
-								base: 'column-reverse', // 0-48em
-								lg: 'row' // 48em-80em,
-							}}
-						>
-							{/* Comment section used to be here, now moved below */}
-							<Flex p={2} flex={1} position={'relative'}>
-								<div id="image" />
-								<ImageDot
-									newComment={newComment}
-									documentType={file.type}
-									removeComment={removeComment}
-									editComment={editComment}
-									updateStatus={updateStatus}
-									imageSrc={file.url}
-									dots={dots}
-									setActivePoint={setActivePoint}
-									activePoint={activePoint}
-									isNextImage={true}
-									isPrevImage={true}
-									prevImage={() => moveFile('left')}
-									nextImage={() => moveFile('right')}
-								/>
+				</ModalHeader>
+				<ModalCloseButton />
+				<ModalBody>
+					<VStack align={'stretch'}>
+						<Divider />
+						{
+							<Flex
+								minHeight={'60vh'}
+								direction={{
+									base: 'column-reverse', // 0-48em
+									lg: 'row' // 48em-80em,
+								}}
+							>
+								{/* Comment section used to be here, now moved below */}
+								<Flex p={2} flex={1} position={'relative'}>
+									<div id="image" />
+									<ImageDot
+										newComment={newComment}
+										documentType={file.type}
+										removeComment={removeComment}
+										editComment={editComment}
+										updateStatus={updateStatus}
+										imageSrc={file.url}
+										dots={dots}
+										setActivePoint={setActivePoint}
+										activePoint={activePoint}
+										isNextImage={true}
+										isPrevImage={true}
+										prevImage={() => moveFile('left')}
+										nextImage={() => moveFile('right')}
+									/>
+								</Flex>
 							</Flex>
-						</Flex>
-					}
-					<Divider />
+						}
+						<Divider />
 
-					{/* Flex box with comments and another box with info and buttons */}
-					<Flex justify={'space-around'}>
-						{/* VStack with Comment info */}
-						<VStack>
-							<Flex direction={'column'} width={'100%'}>
-								<Heading p={2} pl={0}>
-									Notes
-								</Heading>
-								<Box mb={2} p={4} bg={'#EFEFEF'} borderRadius={6}>
-									<ImportantIcon />
-									<Text pt={4}>
-										Click the image to add notes that will be shared with your
-										co-workers
-									</Text>
-								</Box>
+						{/* Flex box with comments and another box with info and buttons */}
+						<Flex justify={'space-around'}>
+							{/* VStack with Comment info */}
+							<VStack>
+								<Flex direction={'column'} width={'100%'}>
+									<Heading p={2} pl={0}>
+										Notes
+									</Heading>
+									<Box mb={2} p={4} bg={'#EFEFEF'} borderRadius={6}>
+										<ImportantIcon />
+										<Text pt={4}>
+											Click the image to add notes that will be shared with
+											your co-workers
+										</Text>
+									</Box>
 
-								{/* List of comments */}
-								{/* I want to add a scroll snap of some sort, when I press a comment, it auto scrolls to the image.  */}
-								<Box overflow={'scroll'} maxHeight={'350px'}>
-									{dots &&
-										dots.map((s) => {
-											if (s.comments.length === 0) {
-												return null;
-											}
-											const currentComment =
-												s.comments[s.comments.length - 1];
+									{/* List of comments */}
+									{/* I want to add a scroll snap of some sort, when I press a comment, it auto scrolls to the image.  */}
+									<Box overflow={'scroll'} maxHeight={'350px'}>
+										{dots &&
+											dots.map((s) => {
+												if (s.comments.length === 0) {
+													return null;
+												}
+												const currentComment =
+													s.comments[s.comments.length - 1];
 
-											return (
-												<>
-													<ScrollIntoView selector="#image">
-														<Flex
-															p={2}
-															py={2}
-															direction={'column'}
-															key={s.dotId}
-															_hover={{ background: '#ececf1' }}
-															cursor={'pointer'}
-															onClick={() => setActivePoint(s.dotId)}
-														>
-															<Flex>
+												return (
+													<React.Fragment key={s.dotId}>
+														<ScrollIntoView selector="#image">
+															<Flex
+																p={2}
+																py={2}
+																direction={'column'}
+																_hover={{ background: '#ececf1' }}
+																cursor={'pointer'}
+																onClick={() =>
+																	setActivePoint(s.dotId)
+																}
+															>
+																<Flex>
+																	<Text
+																		pr={2}
+																		fontSize={'11px'}
+																		isTruncated
+																	>
+																		{currentComment?.userName}
+																	</Text>
+																	<Text
+																		fontSize={'11px'}
+																		isTruncated
+																	>
+																		{formatDate(
+																			new Date(
+																				currentComment?.created ||
+																					new Date()
+																			)
+																		)}
+																	</Text>
+																</Flex>
 																<Text
-																	pr={2}
+																	color={'black'}
+																	fontWeight={'400'}
 																	fontSize={'11px'}
 																	isTruncated
 																>
-																	{currentComment?.userName}
+																	{currentComment.comment}
 																</Text>
-																<Text fontSize={'11px'} isTruncated>
-																	{formatDate(
-																		new Date(
-																			currentComment?.created ||
-																				new Date()
-																		)
+																<Flex>
+																	<Text
+																		fontSize={'11px'}
+																		isTruncated
+																	>
+																		{s.comments.length} comments
+																	</Text>
+																	{s.dotId === activePoint && (
+																		<>
+																			<Spacer />
+																			<Tag size={'sm'}>
+																				Active
+																			</Tag>
+																		</>
 																	)}
-																</Text>
+																</Flex>
 															</Flex>
-															<Text
-																color={'black'}
-																fontWeight={'400'}
-																fontSize={'11px'}
-																isTruncated
-															>
-																{currentComment.comment}
-															</Text>
-															<Flex>
-																<Text fontSize={'11px'} isTruncated>
-																	{s.comments.length} comments
-																</Text>
-																{s.dotId === activePoint && (
-																	<>
-																		<Spacer />
-																		<Tag size={'sm'}>
-																			Active
-																		</Tag>
-																	</>
-																)}
-															</Flex>
-														</Flex>
-														<Divider />
-													</ScrollIntoView>
-												</>
-											);
-										})}
-								</Box>
-							</Flex>
-						</VStack>
-
-						{/* VStack with project info and Buttons */}
-						<VStack align={'stretch'} w={'250px'}>
-							<VStack>
-								<div style={{ height: 20 }} />
-								<HStack justify={'space-between'} align={'center'}>
-									<Heading size={'md'}>Project</Heading>
-									<Text>{project?.name}</Text>
-								</HStack>
-								<HStack justify={'space-between'} align={'center'}>
-									<Heading size={'md'}>Size</Heading>
-									<Text>{humanFileSize(file.bytes)}</Text>
-								</HStack>
-								<HStack justify={'space-between'} align={'center'}>
-									<Heading size={'md'}>Created</Heading>
-									<Text>
-										{DateTime.fromMillis(file.created || 0).toFormat(
-											GANT_CHART_FORMAT
-										)}
-									</Text>
-								</HStack>
-								<div style={{ height: 2 }} />
-							</VStack>
-							<Spacer />
-							{/* Buttons */}
-							<HStack justify={'space-between'} align={'center'}>
-								<VStack justify={'center'} align={'center'}>
-									<IconButton
-										aria-label={'Delete'}
-										colorScheme={'black'}
-										icon={<ShareIcon color={'white'} />}
-										onClick={() => {
-											setModalContext({ shareItem: { file: file } });
-										}}
-									/>
-									<Text color={'black'} fontSize={'l'}>
-										Share
-									</Text>
-								</VStack>
-								<VStack justify={'center'} align={'center'}>
-									<a
-										href={file.url}
-										target={'_blank'}
-										rel={'noopener noreferrer'}
-									>
-										<IconButton
-											aria-label={'Download'}
-											colorScheme={'black'}
-											icon={<DownloadIcon color={'white'} />}
-										/>
-									</a>
-									<Text color={'black'} fontSize={'l'}>
-										Download
-									</Text>
-								</VStack>
-								<VStack justify={'center'} align={'center'}>
-									<ConfirmDialog
-										header={'Delete image'}
-										setIsOpen={setDialogOpen}
-										callback={async (b) => {
-											if (b) {
-												await deleteDocument(file);
-												navigate(
-													`/files/${file.projectId}/${
-														file.folderId > 0 ? file.folderId : ''
-													}`
+															<Divider />
+														</ScrollIntoView>
+													</React.Fragment>
 												);
-											}
-											setDialogOpen(false);
-										}}
-										isOpen={dialogOpen}
-									>
+											})}
+									</Box>
+								</Flex>
+							</VStack>
+
+							{/* VStack with project info and Buttons */}
+							<VStack align={'stretch'} w={'250px'}>
+								<VStack>
+									<div style={{ height: 20 }} />
+									<HStack justify={'space-between'} align={'center'}>
+										<Heading size={'md'}>Project</Heading>
+										<Text>{project?.name}</Text>
+									</HStack>
+									<HStack justify={'space-between'} align={'center'}>
+										<Heading size={'md'}>Size</Heading>
+										<Text>{humanFileSize(file.bytes)}</Text>
+									</HStack>
+									<HStack justify={'space-between'} align={'center'}>
+										<Heading size={'md'}>Created</Heading>
+										<Text>
+											{DateTime.fromMillis(file.created || 0).toFormat(
+												GANT_CHART_FORMAT
+											)}
+										</Text>
+									</HStack>
+									<div style={{ height: 2 }} />
+								</VStack>
+								<Spacer />
+								{/* Buttons */}
+								<HStack justify={'space-between'} align={'center'}>
+									<VStack justify={'center'} align={'center'}>
 										<IconButton
 											aria-label={'Delete'}
-											colorScheme={'red'}
-											icon={<TrashIcon color={'white'} />}
+											colorScheme={'black'}
+											icon={<ShareIcon color={'white'} />}
 											onClick={() => {
-												setDialogOpen(true);
+												setModalContext({ shareItem: { file: file } });
 											}}
 										/>
 										<Text color={'black'} fontSize={'l'}>
-											Delete
+											Share
 										</Text>
-									</ConfirmDialog>
-								</VStack>
-							</HStack>
-							<div style={{ height: 10 }} />
-						</VStack>
-					</Flex>
-				</VStack>
-			</Modal>
-		</>
+									</VStack>
+									<VStack justify={'center'} align={'center'}>
+										<a
+											href={file.url}
+											target={'_blank'}
+											rel={'noopener noreferrer'}
+										>
+											<IconButton
+												aria-label={'Download'}
+												colorScheme={'black'}
+												icon={<DownloadIcon color={'white'} />}
+											/>
+										</a>
+										<Text color={'black'} fontSize={'l'}>
+											Download
+										</Text>
+									</VStack>
+									<VStack justify={'center'} align={'center'}>
+										<ConfirmDialog
+											header={'Delete image'}
+											setIsOpen={setDialogOpen}
+											callback={async (b) => {
+												if (b) {
+													await deleteDocument(file);
+													navigate(
+														`/files/${file.projectId}/${
+															file.folderId > 0 ? file.folderId : ''
+														}`
+													);
+												}
+												setDialogOpen(false);
+											}}
+											isOpen={dialogOpen}
+										>
+											<IconButton
+												aria-label={'Delete'}
+												colorScheme={'red'}
+												icon={<TrashIcon color={'white'} />}
+												onClick={() => {
+													setDialogOpen(true);
+												}}
+											/>
+											<Text color={'black'} fontSize={'l'}>
+												Delete
+											</Text>
+										</ConfirmDialog>
+									</VStack>
+								</HStack>
+								<div style={{ height: 10 }} />
+							</VStack>
+						</Flex>
+					</VStack>
+				</ModalBody>
+			</ModalContent>
+		</Modal>
 	);
 };
--- web/src/pages/Files/new/components/FilesUi.tsx
+++ web/src/pages/Files/new/components/FilesUi.tsx
@@ -1,4 +1,4 @@
-import { HStack, Heading, VStack } from '@chakra-ui/react';
+import { HStack, Heading, VStack, useDisclosure } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { CardBase } from '../../../../components/CardBase';
@@ -10,44 +10,98 @@ import { ProjectImage } from '../../../../models/ProjectImage';
 import { GigoverFile } from '../../components/File';
 
 export const FilesUi = ({ files, title, projectId }) => {
-	const params = useParams();
+	const params = useParams<{ projectId: string; folderId?: string; fileId?: string }>();
 	const [selectedFile, setSelectedFile] = useState<ProjectImage | null>(null);
 	const [selectedIndex, setSelectedIndex] = useState<number>(-1);
 	const navigate = useNavigate();
 
+	const { isOpen, onOpen, onClose: disclosureOnClose } = useDisclosure();
+
 	const folderId = params.folderId || null;
 
 	useEffect(() => {
-		if (files && files.length > 0 && params.fileId) {
-			const index = files.findIndex(
-				(d: ProjectImage) => d.imageId === parseInt(params.fileId || '-1')
-			);
+		const currentFileIdStr = params.fileId;
+		if (currentFileIdStr && files && files.length > 0) {
+			const fileIdToFind = parseInt(currentFileIdStr, 10);
+			const index = files.findIndex((d: ProjectImage) => d.imageId === fileIdToFind);
 			const file = files[index];
 
 			if (file) {
-				setSelectedIndex(index);
 				setSelectedFile(file);
-				return;
+				setSelectedIndex(index);
+				if (!isOpen) {
+					onOpen();
+				}
+			} else {
+				// fileId in URL but not found (e.g., deleted or invalid)
+				setSelectedFile(null);
+				setSelectedIndex(-1);
+				if (isOpen) {
+					disclosureOnClose();
+				}
+				// Navigate away from the invalid fileId URL
+				const basePath = `/files/${projectId}${folderId ? '/folder/' + folderId : ''}`;
+				// Only navigate if params.fileId was actually set, to avoid navigating on initial load without a fileId
+				if (params.fileId) {
+					navigate(basePath, { replace: true });
+				}
+			}
+		} else {
+			// No fileId in URL, or no files array
+			setSelectedFile(null);
+			setSelectedIndex(-1);
+			if (isOpen) {
+				disclosureOnClose();
 			}
 		}
-		setSelectedFile(null);
-		setSelectedIndex(-1);
-	}, [files, params.fileId]);
+	}, [
+		files,
+		params.fileId,
+		isOpen,
+		onOpen,
+		disclosureOnClose,
+		navigate,
+		projectId,
+		folderId,
+		setSelectedFile,
+		setSelectedIndex
+	]);
+
+	const handleModalClose = () => {
+		disclosureOnClose();
+		const basePath = `/files/${projectId}${folderId ? '/folder/' + folderId : ''}`;
+		// Only navigate if we are currently on a specific file path
+		if (params.fileId) {
+			navigate(basePath, { replace: true });
+		}
+	};
 
 	const moveFile = (direction: 'left' | 'right') => {
-		let file;
-		if (direction === 'left') {
-			file = files[selectedIndex - 1];
-		} else {
-			file = files[selectedIndex + 1];
+		let targetFile: ProjectImage | undefined;
+		const currentFolderId = params.folderId || null;
+
+		if (selectedIndex === -1 && files && files.length > 0) {
+			// If no file is selected, but we try to move, default to selecting first/last
+			if (direction === 'left') {
+				targetFile = files[files.length - 1];
+			} else {
+				targetFile = files[0];
+			}
+		} else if (direction === 'left' && selectedIndex > 0) {
+			targetFile = files[selectedIndex - 1];
+		} else if (
+			direction === 'right' &&
+			selectedIndex < files.length - 1 &&
+			selectedIndex !== -1
+		) {
+			targetFile = files[selectedIndex + 1];
 		}
-		if (file) {
+
+		if (targetFile) {
 			navigate(
-				'/files/' +
-					projectId +
-					(folderId ? '/folder/' + folderId : '') +
-					'/' +
-					file.imageId,
+				`/files/${projectId}${currentFolderId ? '/folder/' + currentFolderId : ''}/${
+					targetFile.imageId
+				}`,
 				{ replace: true }
 			);
 		}
@@ -59,12 +113,13 @@ export const FilesUi = ({ files, title, projectId }) => {
 
 	return (
 		<>
-			{selectedFile && (
+			{selectedFile && isOpen && (
 				<EditPhotoModal
 					projectId={projectId}
 					file={selectedFile}
-					onClose={() => navigate(-1)}
+					onClose={handleModalClose}
 					moveFile={moveFile}
+					isOpen={isOpen}
 				/>
 			)}
 
