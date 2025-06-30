c8e4b0badc0cb4529e242708451df6bda735be6d
Einar <einargudnig@gmail.com>
2025-06-30 21:04:38 +0000

    building settings page

--- web/src/pages/Settings/Settings.tsx
+++ web/src/pages/Settings/Settings.tsx
@@ -1,20 +1,27 @@
 import {
-	Flex,
+	Box,
 	Breadcrumb,
 	BreadcrumbItem,
 	BreadcrumbLink,
-	Heading,
+	Button,
+	Flex,
 	HStack,
-	VStack,
-	Box,
-	Text
+	Heading,
+	Text,
+	VStack
 } from '@chakra-ui/react';
-import { DisabledPage } from '../../components/disabled/DisbledPage';
 import { Link as RouterLink } from 'react-router-dom';
+import { DisabledPage } from '../../components/disabled/DisbledPage';
+import { SettingsLayout } from './SettingsLayout';
 
 export function Settings() {
 	const pageTitle = 'Settings';
 	const breadcrumbs = [{ title: 'Settings', url: '/settings' }];
+	const pageActions = (
+		<Box display={'flex'} gap={2} alignItems={'center'} py={2} height={'50px'}>
+			<Button colorScheme={'yellow'}>Manage active organization</Button>
+		</Box>
+	);
 
 	return (
 		<>
@@ -26,11 +33,10 @@ export function Settings() {
 				borderColor="gray.200"
 				boxShadow="6px 6px 25px rgba(0, 0, 0, 0.03)"
 				bg="white"
-				mb={4}
 				px={3}
 			>
 				<Flex justifyContent="space-between" alignItems="center">
-					<Box>
+					<Box mt={1}>
 						{breadcrumbs ? (
 							<Breadcrumb spacing="8px">
 								{breadcrumbs.map((breadcrumb, bIndex) => (
@@ -51,6 +57,7 @@ export function Settings() {
 							</Heading>
 						)}
 					</Box>
+					<HStack spacing={2}>{pageActions}</HStack>
 				</Flex>
 			</Box>
 			<Box p={2}>
@@ -58,7 +65,7 @@ export function Settings() {
 					<VStack style={{ height: '100%' }}>
 						<HStack style={{ flex: 1, height: '100%', width: '100%' }}>
 							<Box overflowY={'auto'} height={'100%'} width={'100%'}>
-								Settings
+								<SettingsLayout />
 							</Box>
 						</HStack>
 					</VStack>
