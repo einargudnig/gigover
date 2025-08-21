17936d8fd682f9e6093ae4c2c399914a2dda8882
Einar <einargudnig@gmail.com>
2025-08-21 09:04:06 +0000

    all big list component use the new dataFetchingBoundary

--- web/src/pages/Procurement/Offers/MyOffersList.tsx
+++ web/src/pages/Procurement/Offers/MyOffersList.tsx
@@ -12,10 +12,15 @@ import {
 import { CardBaseLink } from '../../../components/CardBase';
 import { Offer } from '../../../models/Tender';
 import { useGetUserOffers } from '../../../queries/procurement/useGetUserOffers';
-import { ProcurementListSkeleton } from '../ProcurementListSkeleton';
+import { DataFetchingErrorBoundary } from '../../../components/ErrorBoundary';
+import { ApiService } from '../../../services/ApiService';
+import { useQueryClient } from '@tanstack/react-query';
 
 export const MyOffersList = (): JSX.Element => {
-	const { data, isPending } = useGetUserOffers();
+	const { data, isPending, isError, error } = useGetUserOffers();
+
+	const queryClient = useQueryClient();
+
 	const offers: Offer[] | undefined = data;
 	const noOffers = offers?.length === 0;
 
@@ -25,109 +30,125 @@ export const MyOffersList = (): JSX.Element => {
 				<Heading size={'md'}>Offers that you have submitted</Heading>
 			</Flex>
 
-			{isPending ? (
-				<ProcurementListSkeleton />
-			) : (
-				<>
-					{noOffers ? (
-						<Text>
-							You have not made any offers. Make sure you open a offer before you add
-							to it.
-						</Text>
-					) : (
-						<>
-							{offers
-								?.slice()
-								.reverse()
-								.map((o) => {
-									let offerStatus;
-									let statusColor;
-									if (o.status === 0) {
-										offerStatus = 'Unpublished';
-									} else if (o.status === 1) {
-										offerStatus = 'Published';
-									} else if (o.status === 2) {
-										offerStatus = 'Accepted';
-										statusColor = 'green';
-									} else if (o.status === 3) {
-										offerStatus = 'Rejected';
-										statusColor = 'red';
-									} else {
-										offerStatus = 'Unknown';
-									}
+			<DataFetchingErrorBoundary
+				name="MyOffersList"
+				apiEndpoint={ApiService.userOffers}
+				loadingState={isPending}
+				onRetry={() => {
+					queryClient.invalidateQueries({ queryKey: [ApiService.userOffers] });
+				}}
+			>
+				{isError ? (
+					(() => {
+						throw error;
+					})()
+				) : (
+					<>
+						{noOffers ? (
+							<Text>
+								You have not made any offers. Make sure you open a offer before you
+								add to it.
+							</Text>
+						) : (
+							<>
+								{offers
+									?.slice()
+									.reverse()
+									.map((o) => {
+										let offerStatus;
+										let statusColor;
+										if (o.status === 0) {
+											offerStatus = 'Unpublished';
+										} else if (o.status === 1) {
+											offerStatus = 'Published';
+										} else if (o.status === 2) {
+											offerStatus = 'Accepted';
+											statusColor = 'green';
+										} else if (o.status === 3) {
+											offerStatus = 'Rejected';
+											statusColor = 'red';
+										} else {
+											offerStatus = 'Unknown';
+										}
 
-									return (
-										<LinkBox
-											as={CardBaseLink}
-											to={`/tender/my-offer/${o.tenderId}/${o.offerId}`}
-											key={o.offerId}
-											w="100%"
-											maxW="100%"
-											h="auto"
-											mb="8px"
-											mt="8px"
-											sx={{
-												h3: {
-													marginBottom: '16px',
-													color: '#000'
-												},
-												'@media screen and (max-width: 768px)': {
-													width: '100%'
-												}
-											}}
-										>
-											<LinkOverlay
-												href={`/tender/my-offer/${o.tenderId}/${o.offerId}`}
+										return (
+											<LinkBox
+												as={CardBaseLink}
+												to={`/tender/my-offer/${o.tenderId}/${o.offerId}`}
+												key={o.offerId}
+												w="100%"
+												maxW="100%"
+												h="auto"
+												mb="8px"
+												mt="8px"
+												sx={{
+													h3: {
+														marginBottom: '16px',
+														color: '#000'
+													},
+													'@media screen and (max-width: 768px)': {
+														width: '100%'
+													}
+												}}
 											>
-												<Flex direction={'column'}>
-													<Grid templateColumns="repeat(4, 1fr)" gap={1}>
-														<GridItem colSpan={2}>
-															<HStack>
-																<Text as={'b'}>Offer notes:</Text>
-																<Text color={'black'}>
-																	{o.notes}
-																</Text>
-															</HStack>
-															<HStack>
-																<Text as={'b'}>
-																	Tender description:
-																</Text>
-																<Text color={'black'}>
-																	{o.tender.description}
-																</Text>
-															</HStack>
-														</GridItem>
-														<GridItem colSpan={1} />
+												<LinkOverlay
+													href={`/tender/my-offer/${o.tenderId}/${o.offerId}`}
+												>
+													<Flex direction={'column'}>
+														<Grid
+															templateColumns="repeat(4, 1fr)"
+															gap={1}
+														>
+															<GridItem colSpan={2}>
+																<HStack>
+																	<Text as={'b'}>
+																		Offer notes:
+																	</Text>
+																	<Text color={'black'}>
+																		{o.notes}
+																	</Text>
+																</HStack>
+																<HStack>
+																	<Text as={'b'}>
+																		Tender description:
+																	</Text>
+																	<Text color={'black'}>
+																		{o.tender.description}
+																	</Text>
+																</HStack>
+															</GridItem>
+															<GridItem colSpan={1} />
 
-														<GridItem colSpan={1}>
-															<HStack>
-																<Text as={'b'} fontSize={'lg'}>
-																	Offer status:
-																</Text>
-																<Text
-																	color={statusColor}
-																	fontSize={'xl'}
-																>
-																	{offerStatus}
-																</Text>
-															</HStack>
-															<HStack>
-																<Text as={'b'}>Tender</Text>
-																<Text as={'b'}>owner:</Text>
+															<GridItem colSpan={1}>
+																<HStack>
+																	<Text as={'b'} fontSize={'lg'}>
+																		Offer status:
+																	</Text>
+																	<Text
+																		color={statusColor}
+																		fontSize={'xl'}
+																	>
+																		{offerStatus}
+																	</Text>
+																</HStack>
+																<HStack>
+																	<Text as={'b'}>Tender</Text>
+																	<Text as={'b'}>owner:</Text>
 
-																<Text>{o.tender.email}</Text>
-															</HStack>
-														</GridItem>
-													</Grid>
-												</Flex>
-											</LinkOverlay>
-										</LinkBox>
-									);
-								})}
-						</>
-					)}
-				</>
-			)}
+																	<Text>{o.tender.email}</Text>
+																</HStack>
+															</GridItem>
+														</Grid>
+													</Flex>
+												</LinkOverlay>
+											</LinkBox>
+										);
+									})}
+							</>
+						)}
+					</>
+				)}
+			</DataFetchingErrorBoundary>
 		</Box>
 	);
 };
