7c88dd878d5707eeff432c967f93829a01679c50
Einar <einargudnig@gmail.com>
2025-08-22 21:14:41 +0000

    removing error testing stuff, starting to build form boundaries

--- web/src/components/modals/PropertyModals/AddPropertyModal.tsx
+++ web/src/components/modals/PropertyModals/AddPropertyModal.tsx
@@ -1,46 +1,24 @@
-import { Box, Button, FormControl, FormLabel, Input, Text, VStack } from '@chakra-ui/react';
+import { Box, FormControl, FormLabel, Input, Text, VStack } from '@chakra-ui/react';
 import { useForm } from 'react-hook-form';
 import { useCloseModal } from '../../../hooks/useCloseModal';
 import { IPropertyForm } from '../../../models/Property';
 import { useAddProperty } from '../../../mutations/properties/useAddProperty';
 import { FormActions } from '../../FormActions';
 import { FormErrorBoundary, ModalErrorBoundary } from '../../ErrorBoundary';
-import { useState } from 'react';
 
 interface PropertyModalProps {
 	property?: IPropertyForm;
 }
 
 export const AddPropertyModal = ({ property }: PropertyModalProps): JSX.Element => {
-	console.log(property);
 	const closeModal = useCloseModal();
 
-	// Add a state to trigger error for testing
-	const [triggerError, setTriggerError] = useState(false);
-
-	// Test function to cause error in the parent component
-	if (triggerError) {
-		throw new Error('Test error in AddPropertyModal component');
-	}
-
 	return (
-		<>
-			{/* Test button outside error boundaries */}
-			<Box mb={4} p={2} borderWidth="1px" borderRadius="md" bg="yellow.50">
-				<Text fontSize="sm" mb={2}>
-					Error Boundary Testing Controls:
-				</Text>
-				<Button colorScheme="red" size="sm" onClick={() => setTriggerError(true)}>
-					Test Parent Error
-				</Button>
-			</Box>
-
-			<ModalErrorBoundary modalName="AddPropertyModal" onClose={closeModal}>
-				<FormErrorBoundary formName="PropertyForm" preserveFormData={true}>
-					<PropertyForm property={property} closeModal={closeModal} />
-				</FormErrorBoundary>
-			</ModalErrorBoundary>
-		</>
+		<ModalErrorBoundary modalName="AddPropertyModal" onClose={closeModal}>
+			<FormErrorBoundary formName="PropertyForm" preserveFormData={true}>
+				<PropertyForm property={property} closeModal={closeModal} />
+			</FormErrorBoundary>
+		</ModalErrorBoundary>
 	);
 };
 
@@ -48,11 +26,7 @@ const PropertyForm = ({
 	property,
 	closeModal
 }: PropertyModalProps & { closeModal: () => void }): JSX.Element => {
-	// Use the mutation with proper error handling
 	const { mutate: addProperty, isError, error } = useAddProperty();
-
-	// Add state for form error testing
-	const [formErrorTest, setFormErrorTest] = useState(false);
 
 	const {
 		register,
@@ -69,13 +43,7 @@ const PropertyForm = ({
 		throw error;
 	}
 
-	// This will cause an error during render that will be caught by error boundaries
-	if (formErrorTest) {
-		throw new Error('Form test error during render phase');
-	}
-
 	const onSubmit = handleSubmit(async ({ name, address, zipCode, city, country, size, type }) => {
-		// Remove try/catch so errors can propagate to the error boundary
 		addProperty(
 			{
 				name,
@@ -101,42 +69,6 @@ const PropertyForm = ({
 
 	return (
 		<form onSubmit={onSubmit}>
-			{/* Error boundary testing controls */}
-			<Box mb={4} p={2} borderWidth="1px" borderRadius="md" bg="red.50">
-				<Text fontSize="sm" mb={2}>
-					Form Error Testing:
-				</Text>
-				<Button
-					mr={2}
-					colorScheme="red"
-					size="sm"
-					onClick={() => {
-						// This sets state to trigger an error during the next render cycle
-						// which will be caught by the error boundary
-						setFormErrorTest(true);
-					}}
-				>
-					Test Form Error
-				</Button>
-
-				<Button
-					colorScheme="orange"
-					size="sm"
-					onClick={() => {
-						// For React Query errors, we need a way to force a re-render with error state
-						// This is a simple approach that demonstrates the concept
-						addProperty({} as any, {
-							onError: (e) => {
-								console.error('Forced API error for testing:', e);
-								// No need to explicitly throw here - the isError and error state
-								// will be set by React Query, which will trigger our check on next render
-							}
-						});
-					}}
-				>
-					Test API Error
-				</Button>
-			</Box>
 			<VStack mb={-6} align={'stretch'}>
 				<Text>
 					Create a property here. You can add more info after you creat the property.
